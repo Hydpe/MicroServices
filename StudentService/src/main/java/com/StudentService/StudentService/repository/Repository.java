47aec7aa8c4bf9559628c5e1cd4c8d0fb76f5a4d
SHAIK_HYDER_ALI <125179512+Hydpe@users.noreply.github.com>
2025-11-25 12:01:21 +0530

    Delete StudentService directory

--- StudentService/src/main/java/com/StudentService/StudentService/repository/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.StudentService.StudentService.repository;
-
-import com.StudentService.StudentService.Enitity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<Student,Long> {
-    Student findByEmail(String email);
-}
